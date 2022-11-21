ee2c2786337339200f54e0b982b81a65987fe7de
apollosport <apollosport84@gmail.com>
2022-11-21 18:19:32 +0100

    start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { client } from "./client";
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    client
+      .getEntries()
+      .then((res) => setRecipes(res.items))
+      .catch((err) => console.log(err));
+  }, []);
+
+  console.log(recipes);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Leeeeets Cooook!</h1>
+        {recipes &&
+          recipes.map((recipe) => (
+            <div>
+              <h1>{recipe.fields.title}</h1>
+              <span>Rating: {recipe.fields.rating}</span>
+              <img
+                src={recipe.fields.recipeImage.fields.file.url}
+                height="400"
+              />
+              <p>{recipe.fields.description}</p>
+            </div>
+          ))}
       </header>
     </div>
   );
